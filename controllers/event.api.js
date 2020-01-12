9864c48391711cf5714eafb14022abd1345aceee
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-01-11 22:41:55 -0500

    merged stubhub, ticketmaster and seatgeeks

--- controllers/event.api.js
+++ controllers/event.api.js
@@ -3,18 +3,24 @@ const axios = require('axios')
 module.exports = {
     async eventHandler(req, res) {
         const seatGeek = async () => await axios.get("https://api.seatgeek.com/2/events?venue.city=west palm beach&client_id=MjAyODAzODV8MTU3ODc3NDkyNi41")
-            .catch(err => console.log(err))
+            .catch(err => new Error(err))
         const ticketMaster = async () => await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?&city=west palm beach`, { params: { "apikey": "IAF49wzCrcjsCkSgLMKAVWJ5flePU6Vh", } })
-            .catch(err => console.log(err))
+            .catch(err => new Error(err))
+
+        const stubHub = async () => await axios.get('https://api.stubhub.com/sellers/search/events/v3?city=west palm beach', {
+            headers: { "Authorization": "Bearer " + "LAMsjgt0wHACRuEdYHayTNZzsSM6" }
+        })
 
 
         await axios.all(
             [
                 seatGeek(),
-                ticketMaster()
+                ticketMaster(),
+                stubHub()
             ]
         )
-            .then(axios.spread((seats, tix) => {
+            .then(axios.spread((seats, tix, stub) => {
+                const stubEvents = stub.data.events
                 const { events } = tix.data._embedded
                 const evs = seats.data.events
 
@@ -37,13 +43,23 @@ module.exports = {
                         id: event.id,
                         name: event.title,
                         url: event.url,
-                        venue: event.venue.address,
+                        address: event.venue.address,
                         venue_name: event.venue.name,
                         image: event.performers[0].image
                     }
                 })
 
-                const merged = [ticketmaster, geeks]
+                const stubs = stubEvents.map(event => {
+                    return {
+                        id: event.id,
+                        name: event.name,
+                        address: `${event.venue.city} ${event.venue.state}`,
+                        venue_name: event.venue.name
+
+                    }
+                })
+
+                const merged = [ticketmaster, geeks, stubs]
                 res.json(merged)
 
             }))
